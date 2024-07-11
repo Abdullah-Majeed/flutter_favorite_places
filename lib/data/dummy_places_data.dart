a8bb68807e76acd62f646a401b4e11f98b22253a
Abdullah Majeed <abdullahmajeed969@gmail.com>
2024-07-11 23:43:06 +0500

    new flutter app for places

---
+++ lib/data/dummy_places_data.dart
@@ -0,0 +1,7 @@
+import 'package:favorite_places/models/place.dart';
+
+const dummyPlaces = [
+   Place(id: 'p1', title: 'Karachi'),
+   Place(id: 'p2', title: 'Islamabad'),
+   Place(id: 'p3', title: 'Lahore'),
+];
---
+++ lib/screens/tabs.dart
@@ -0,0 +1,56 @@
+import 'package:favorite_places/screens/add_new_places.dart';
+import 'package:flutter/material.dart';
+import 'package:favorite_places/models/place.dart';
+
+class TabScreen extends StatefulWidget {
+  const TabScreen({super.key});
+
+  @override
+  State<TabScreen> createState() => _TabScreenState();
+}
+
+class _TabScreenState extends State<TabScreen> {
+  final List<Place> placesList = [];
+
+  void _addNewPlace(BuildContext context) async {
+    final newPlaceData = await Navigator.of(context)
+        .push<Place>(MaterialPageRoute(builder: (ctx) => const AddNewPlace()));
+    if (newPlaceData == null) {
+      return;
+    }
+    setState(() {
+      placesList.add(newPlaceData);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No places found, try adding some!'),
+    );
+
+    if (placesList.isNotEmpty) {
+      mainContent = ListView.builder(
+          itemCount: placesList.length,
+          itemBuilder: ((context, index) {
+            return ListTile(
+              title: Text(placesList[index].title),
+            );
+          }));
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _addNewPlace(context);
+            },
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: mainContent,
+    );
+  }
+}
